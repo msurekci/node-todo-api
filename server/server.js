d1a83d5e7e91331e4ea9508536fdf2ef95bb7d3d
thisdotrob <robstevenson000@gmail.com>
2017-02-27 21:16:07 +0000

    add new /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -23,12 +24,28 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({ todos });
   }).catch(e => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send({ todo });
+    }
+
+    res.status(404).send();
+  }).catch(e => res.status(404).send(e));
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
